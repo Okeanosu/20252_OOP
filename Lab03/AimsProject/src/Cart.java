3d67263dbbe224eebd04a8052b205fc3e475381c
Okeanosu <phanhaiduong123@gmail.com>
2026-04-17 15:48:33 +0700

    Overloading Method

--- Lab03/AimsProject/src/Cart.java
+++ Lab03/AimsProject/src/Cart.java
@@ -2,7 +2,7 @@ public class Cart {
     public static final int MAX_NUMBERS_ORDERED = 20;
     private final DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
     private int qtyOrdered = 0;
-    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+ /*    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
         if (qtyOrdered < MAX_NUMBERS_ORDERED) {
             itemsOrdered[qtyOrdered] = disc;
             qtyOrdered++;
@@ -10,8 +10,8 @@ public class Cart {
         } else {
             System.out.println("The cart is full. Cannot add more discs.");
         }
-    }
-/*  public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
+    }*/
+  public void addDigitalVideoDisc(DigitalVideoDisc[] dvdList) {
         for (DigitalVideoDisc disc : dvdList) {
             if (qtyOrdered < MAX_NUMBERS_ORDERED) {
                 itemsOrdered[qtyOrdered] = disc;
@@ -22,7 +22,7 @@ public class Cart {
                 break;
             }
         }
-    }*/
+    }
     public void addDigitalVideoDisc(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
         addDigitalVideoDisc(new DigitalVideoDisc[]{dvd1, dvd2});
     }
